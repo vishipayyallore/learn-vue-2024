5b6795ffa079749698e1d0b753101596da778fe1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-22 08:38:51 +0530

    Swamy/21dec work (#1)
    * Update README.md
    
    * Initial Version
    
    * Renamed the file
    
    * Update App.vue
    
    * Code Refactoring

---
+++ build-a-bot/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li class="nav-item">
+          <img class="logo" src="./assets/build-a-bot-logo.png" aria-hidden="true" alt="logo" />
+          Build a Bot
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <HomePage msg="Welcome to Your Vue.js App" />
+  </main>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomePage,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1400px;
+  min-height: 300px;
+}
+
+header {
+  background-color: #999;
+  width: 1460px;
+  color: white;
+  margin: 0 auto;
+}
+
+ul {
+  padding: 3px;
+  display: flex;
+}
+
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+
+.logo {
+  height: 30px;
+  vertical-align: middle;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ build-a-bot/src/components/HomePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png" aria-hidden="true" alt="Robot" />
+    </div>
+    <div class="get-started">
+      <a href="">Get Started</a> building your first bot!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home
+{
+  text-align: center;
+  color: #2c3e50;
+}
+
+.robot
+{
+  width: 300px;
+  height: 300px;
+}
+
+.get-started
+{
+  font-size: 2em;
+  padding-top: 20px;
+}
+</style>
