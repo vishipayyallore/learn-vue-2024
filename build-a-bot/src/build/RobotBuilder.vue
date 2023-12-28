fa8ffd2288bee4223ec8d72f97c4d7d7bebff0db
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-28 10:20:30 +0530

    Changes

--- build-a-bot/src/build/RobotBuilder.vue
+++ build-a-bot/src/build/RobotBuilder.vue
@@ -1,157 +1,159 @@
 <template>
-    <div>
-        <div class="top-row">
-            <div class="top part">
-                <img src="./images/head-big-eye.png" alt="head" />
-                <button class="prev-selector">&#9668;</button>
-                <button class="next-selector">&#9658;</button>
-            </div>
-        </div>
-        <div class="middle-row">
-            <div class="left part">
-                <img src="./images/arm-articulated-claw.png" alt="left arm" />
-                <button class="prev-selector">&#9650;</button>
-                <button class="next-selector">&#9660;</button>
-            </div>
-            <div class="center part">
-                <img src="./images/torso-flexible-gauged.png" alt="torso" />
-                <button class="prev-selector">&#9668;</button>
-                <button class="next-selector">&#9658;</button>
-            </div>
-            <div class="right part">
-                <img src="./images/arm-dual-claw.png" alt="right arm" />
-                <button class="prev-selector">&#9650;</button>
-                <button class="next-selector">&#9660;</button>
-            </div>
-        </div>
-        <div class="bottom-row">
-            <div class="bottom part">
-                <img src="./images/base-single-wheel.png" alt="base" />
-                <button class="prev-selector">&#9668;</button>
-                <button class="next-selector">&#9658;</button>
-            </div>
-        </div>
+  <div>
+    <div class="top-row">
+      <div class="top part">
+        <img v-bind:src="availableParts.heads[0].imageUrl" alt="head" />
+        <button class="prev-selector">&#9668;</button>
+        <button class="next-selector">&#9658;</button>
+      </div>
     </div>
+    <div class="middle-row">
+      <div class="left part">
+        <img v-bind:src="availableParts.arms[0].imageUrl" alt="left arm" />
+        <button class="prev-selector">&#9650;</button>
+        <button class="next-selector">&#9660;</button>
+      </div>
+      <div class="center part">
+        <img v-bind:src="availableParts.torsos[0].imageUrl" alt="torso" />
+        <button class="prev-selector">&#9668;</button>
+        <button class="next-selector">&#9658;</button>
+      </div>
+      <div class="right part">
+        <img v-bind:src="availableParts.arms[0].imageUrl" alt="right arm" />
+        <button class="prev-selector">&#9650;</button>
+        <button class="next-selector">&#9660;</button>
+      </div>
+    </div>
+    <div class="bottom-row">
+      <div class="bottom part">
+        <img v-bind:src="availableParts.bases[0].imageUrl" alt="base" />
+        <button class="prev-selector">&#9668;</button>
+        <button class="next-selector">&#9658;</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import parts from '../data/parts';
+
 export default {
-    name: 'RobotBuilder',
+  name: 'RobotBuilder',
 
-    data() {
-        return {
-
-        };
-    },
+  data() {
+    return {
+      availableParts: parts,
+    };
+  },
 };
 </script>
 
 <style>
 .part {
-    position: relative;
-    width: 200px;
-    height: 200px;
-    border: 3px solid #aaa;
+  position: relative;
+  width: 200px;
+  height: 200px;
+  border: 3px solid #aaa;
 }
 
 .part img {
-    width: 200px;
+  width: 200px;
 }
 
 .top-row {
-    display: flex;
-    justify-content: space-around;
+  display: flex;
+  justify-content: space-around;
 }
 
 .middle-row {
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 
 .bottom-row {
-    display: flex;
-    justify-content: space-around;
-    border-top: none;
+  display: flex;
+  justify-content: space-around;
+  border-top: none;
 }
 
 .top {
-    border-bottom: none;
+  border-bottom: none;
 }
 
 .left {
-    border-right: none;
+  border-right: none;
 }
 
 .right {
-    border-left: none;
+  border-left: none;
 }
 
 .left img {
-    transform: rotate(-90deg);
+  transform: rotate(-90deg);
 }
 
 .right img {
-    transform: rotate(90deg);
+  transform: rotate(90deg);
 }
 
 .bottom {
-    border-top: none;
+  border-top: none;
 }
 
 .prev-selector {
-    position: absolute;
-    z-index: 1;
-    top: -3px;
-    left: -28px;
-    width: 25px;
-    height: 206px;
+  position: absolute;
+  z-index: 1;
+  top: -3px;
+  left: -28px;
+  width: 25px;
+  height: 206px;
 }
 
 .next-selector {
-    position: absolute;
-    z-index: 1;
-    top: -3px;
-    right: -28px;
-    width: 25px;
-    height: 206px;
+  position: absolute;
+  z-index: 1;
+  top: -3px;
+  right: -28px;
+  width: 25px;
+  height: 206px;
 }
 
 .center .prev-selector,
 .center .next-selector {
-    opacity: 0.8;
+  opacity: 0.8;
 }
 
 .left .prev-selector {
-    top: -28px;
-    left: -3px;
-    width: 179px;
-    height: 25px;
+  top: -28px;
+  left: -3px;
+  width: 179px;
+  height: 25px;
 }
 
 .left .next-selector {
-    top: auto;
-    bottom: -28px;
-    left: -3px;
-    width: 179px;
-    height: 25px;
+  top: auto;
+  bottom: -28px;
+  left: -3px;
+  width: 179px;
+  height: 25px;
 }
 
 .right .prev-selector {
-    top: -28px;
-    left: 24px;
-    width: 179px;
-    height: 25px;
+  top: -28px;
+  left: 24px;
+  width: 179px;
+  height: 25px;
 }
 
 .right .next-selector {
-    top: auto;
-    bottom: -28px;
-    left: 24px;
-    width: 179px;
-    height: 25px;
+  top: auto;
+  bottom: -28px;
+  left: 24px;
+  width: 179px;
+  height: 25px;
 }
 
 .right .next-selector {
-    right: -3px;
+  right: -3px;
 }
 </style>
